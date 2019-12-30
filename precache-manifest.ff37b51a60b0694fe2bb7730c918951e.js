20f7579b2bf1e5518288fd9f679467bd5a3d45e7
DonnyWork <qwe54188@gmail.com>
2019-12-30 17:29:05 +0800

    Updates

---
+++ precache-manifest.ff37b51a60b0694fe2bb7730c918951e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32ecb1b8d2efecb74ea2108031af739",
+    "url": "/Donnywork/index.html"
+  },
+  {
+    "revision": "58ca0cd14bf50c197487",
+    "url": "/Donnywork/static/css/main.dfab033e.chunk.css"
+  },
+  {
+    "revision": "915911023f648c4d792c",
+    "url": "/Donnywork/static/js/2.ca551dd4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Donnywork/static/js/2.ca551dd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58ca0cd14bf50c197487",
+    "url": "/Donnywork/static/js/main.881b2e87.chunk.js"
+  },
+  {
+    "revision": "a3c4e44cef57a0cfb702",
+    "url": "/Donnywork/static/js/runtime-main.98f172d8.js"
+  },
+  {
+    "revision": "52ea13289200abe983a4d41e4a9d29dc",
+    "url": "/Donnywork/static/media/app_1.52ea1328.png"
+  },
+  {
+    "revision": "5f8070b5ccedaa3450d27a11ffff182d",
+    "url": "/Donnywork/static/media/app_2.5f8070b5.png"
+  },
+  {
+    "revision": "49f864b1ac820752047ccc2503c5350c",
+    "url": "/Donnywork/static/media/app_3.49f864b1.png"
+  },
+  {
+    "revision": "d780e9b16e05d8431e691d6a82b594c4",
+    "url": "/Donnywork/static/media/app_4.d780e9b1.png"
+  },
+  {
+    "revision": "bfe4de5af62ac31f88e9f2c39e67e7b6",
+    "url": "/Donnywork/static/media/app_5.bfe4de5a.png"
+  },
+  {
+    "revision": "bff6e6481ac73d89cde5bb2139f5e6fb",
+    "url": "/Donnywork/static/media/app_6.bff6e648.png"
+  },
+  {
+    "revision": "680c2c49bbd1fb1014acf51b787dc3e8",
+    "url": "/Donnywork/static/media/app_7.680c2c49.png"
+  },
+  {
+    "revision": "d8a5258777b23da1e03e03d0d5d733b0",
+    "url": "/Donnywork/static/media/header-02.d8a52587.png"
+  },
+  {
+    "revision": "8c30bc53309ac0fddea4bb4f1be704bf",
+    "url": "/Donnywork/static/media/header-03.8c30bc53.png"
+  },
+  {
+    "revision": "5eb22568c755475e009214a45209bd3b",
+    "url": "/Donnywork/static/media/header-04.5eb22568.png"
+  },
+  {
+    "revision": "cdeaed55bad1a3baf85bca094751c753",
+    "url": "/Donnywork/static/media/header-05.cdeaed55.png"
+  },
+  {
+    "revision": "b0c533e1c97a9ba757ba9afbdde08ffc",
+    "url": "/Donnywork/static/media/header-06.b0c533e1.png"
+  },
+  {
+    "revision": "99c2163206442e290558dc3d832ea0ee",
+    "url": "/Donnywork/static/media/header-07.99c21632.png"
+  },
+  {
+    "revision": "e8b3228b5ffdc87cca4f180463cfd231",
+    "url": "/Donnywork/static/media/header-08.e8b3228b.png"
+  },
+  {
+    "revision": "7a0232570ed42ba422da39b436f99242",
+    "url": "/Donnywork/static/media/header-09.7a023257.png"
+  },
+  {
+    "revision": "6f53b69038db9c678a156d1319772a6b",
+    "url": "/Donnywork/static/media/user.6f53b690.png"
+  },
+  {
+    "revision": "455ec3172481e28d7414714870a9434a",
+    "url": "/Donnywork/static/media/works_app1.455ec317.jpg"
+  },
+  {
+    "revision": "d7281bd1c78a5185227cd05095486c5c",
+    "url": "/Donnywork/static/media/works_app2.d7281bd1.jpg"
+  },
+  {
+    "revision": "4e0ee1382ce7c0829af3d9a4069563ab",
+    "url": "/Donnywork/static/media/works_app3.4e0ee138.jpg"
+  },
+  {
+    "revision": "4649fd8443eeea4bdc7d8e18b94154c3",
+    "url": "/Donnywork/static/media/works_app4.4649fd84.jpg"
+  },
+  {
+    "revision": "b9490db24faff81cbc027fe3bf67df66",
+    "url": "/Donnywork/static/media/works_app5.b9490db2.png"
+  },
+  {
+    "revision": "b7ecfc899ea417d73af97e0b32b8e584",
+    "url": "/Donnywork/static/media/works_app6.b7ecfc89.jpg"
+  },
+  {
+    "revision": "dc5e5579a80b8c61e46f4e3f546a4661",
+    "url": "/Donnywork/static/media/works_app7.dc5e5579.png"
+  }
+]);
