4f2a6bf8474c6842cc0b880b73c59ba79d842dc9
DonnyWork <qwe54188@gmail.com>
2020-01-09 10:15:56 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Donnywork/precache-manifest.6c2d4ce0154c4c2ff9039eb98758db6b.js"
+  "/Donnywork/precache-manifest.213b1ecb716e15c2e2d7a2c7de039bb9.js"
 );
 
 self.addEventListener('message', (event) => {
